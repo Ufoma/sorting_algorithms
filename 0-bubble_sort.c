7f08c4624668a32b327a142b8f373df326b815a5
Ufoma <dymondintheski@gmail.com>
2024-02-21 00:47:37 +0100

    bubble sort algorithm

---
+++ 0-bubble_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sort array elements in ascending order
+ * @array - array to be sorted
+ * @size - size of the array
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t ind;
+	int temp; /* temporary variable for swaps */
+	if (size < 2) 
+/* Check if array size is less than 2 */
+	return;
+
+for (size_t i = 0; i < size; i++)
+	/* outer loop */
+{
+	for (ind = 0; ind < size - i - 1; ind++)
+		/*inner loop for swapping*/
+	{
+		if (array[ind] > array[ind + 1])
+            {
+		    /*  Swap elements */
+		    temp = array[ind];
+		    array[ind] = array[ind + 1];
+		    array[ind + 1] = temp;
+
+		    /* Print array after each swap */
+		    print_array(array, size);
+            }
+        }
+    }
+}
--- sort.h
+++ sort.h
@@ -1,3 +1,6 @@
+#ifndef SORT_H
+#define SORT_H
+
 #include <stdio.h>
 
 /**
@@ -14,7 +17,14 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+void bubble_sort(int *array, size_t size);
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+void insertion_sort_list(listint_t **list);
+listint_t *swap_node(listint_t *node, listint_t **list);
 void selection_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void quick_sort(int *array, size_t size);
+
+#endif
