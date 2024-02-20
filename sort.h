0fdcbe60d36b6eacc477f24d2c120b91da40be03
chijioke <chijiokelonginus4@gmail.com>
2024-02-20 22:32:25 +0100

    added quick_sort algoritm

--- sort.h
+++ sort.h
@@ -17,3 +17,4 @@ typedef struct listint_s
 void selection_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+void quick_sort(int *array, size_t size);
